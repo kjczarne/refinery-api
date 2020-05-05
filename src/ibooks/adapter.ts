e4219c516bf6fde497f86d66e5ac1d211d87a3e1
Krzysztof Czarnecki <kjczarne@Krzysztofs-MBP.home>
2020-05-05 17:20:37 +0200

    feat: modified to show possibility of Anki-iBooks interoperability

--- src/ibooks/adapter.ts
+++ src/ibooks/adapter.ts
@@ -1,8 +1,10 @@
 import * as sqlite from 'sqlite3';
 import { IRecord } from '../interfaces';
-import { constructRecord, convertToMarkdown, convertToHtml, sqlQueryRun } from '../engine';
+import { constructRecord, convertToMarkdown, convertToHtml, sqlQueryRun, convertToFlashcard } from '../engine';
 import { readFileSync } from 'fs';
 import * as yaml from 'yaml';
+import { AnkiEngine } from '../anki/ankiEngine';
+import { models } from '../anki/ankiObjects';
 
 let envFile: string = readFileSync('./src/ibooks/env.yaml', 'utf8');
 let envObj: any = yaml.parse(envFile);
@@ -30,10 +32,23 @@ FROM ZAEANNOTATION
 
 let x = sqlQueryRun(db, sqlQuery);
 
+
+
+
+let db2: sqlite.Database = new sqlite.Database('sranki.sqlite');
+let schema: Array<string> = readFileSync('./src/anki/ankiDbSchema.sql').toString().split(';\n');
+for (let i = 0; i<schema.length; i++){
+    schema[i] += ';'
+}
+let now: number = Date.now().valueOf();
+let exporter = new AnkiEngine('heck_deck', now, schema, db2, models);
+
 x.then((response) => {
     for (let rec of response){
         let record: IRecord = constructRecord("epubcfi", rec.pagemap, rec.origtext, rec.note);
+        convertToFlashcard(record, exporter);
         records.push(record);
     }
-    console.log(records);
+    // console.log(records);
+    db.close((err)=>{console.log(err?.message)});
 });

fede9ab36694d061e7da07f188efa3df223c779c
Krzysztof Czarnecki <kjczarne@Krzysztofs-MBP.home>
2020-05-07 00:56:08 +0200

    fix: transforming iBooks highlights to Anki Flashcards

--- src/ibooks/adapter.ts
+++ src/ibooks/adapter.ts
@@ -1,10 +1,11 @@
 import * as sqlite from 'sqlite3';
 import { IRecord } from '../interfaces';
-import { constructRecord, convertToMarkdown, convertToHtml, sqlQueryRun, convertToFlashcard } from '../engine';
+import { constructRecord, convertToMarkdown, convertToHtml, sqlQueryRun, convertToFlashcard, constructRecords } from '../engine';
 import { readFileSync } from 'fs';
 import * as yaml from 'yaml';
 import { AnkiEngine } from '../anki/ankiEngine';
 import { models } from '../anki/ankiObjects';
+import { delay } from '../utils';
 
 let envFile: string = readFileSync('./src/ibooks/env.yaml', 'utf8');
 let envObj: any = yaml.parse(envFile);
@@ -13,14 +14,7 @@ let pathToDb: string = envObj.ibooks.db_dir;
 let fileName: string = envObj.ibooks.db_file;
 let fullPath: string = pathToDb + fileName;
 
-let db = new sqlite.Database(fullPath, (err) => {
-    if (err) {
-      console.error(err.message);
-    }
-    console.log('Connected to the iBooks Annotations database.');
-});
 
-let records: Array<IRecord> = [];
 
 let sqlQuery: string = `
 SELECT
@@ -30,25 +24,17 @@ ZANNOTATIONSELECTEDTEXT AS origtext
 FROM ZAEANNOTATION
 `
 
-let x = sqlQueryRun(db, sqlQuery);
+let x = sqlQueryRun(fullPath, sqlQuery);
 
-
-
-
-let db2: sqlite.Database = new sqlite.Database('sranki.sqlite');
-let schema: Array<string> = readFileSync('./src/anki/ankiDbSchema.sql').toString().split(';\n');
-for (let i = 0; i<schema.length; i++){
-    schema[i] += ';'
-}
+let db2Path: string = 'sranki.sqlite';
+let schema: string = readFileSync('./src/anki/ankiDbSchema.sql').toString()
 let now: number = Date.now().valueOf();
-let exporter = new AnkiEngine('heck_deck', now, schema, db2, models);
+let exporter = new AnkiEngine('heck_deck', now, schema, db2Path, models);
 
-x.then((response) => {
-    for (let rec of response){
-        let record: IRecord = constructRecord("epubcfi", rec.pagemap, rec.origtext, rec.note);
-        convertToFlashcard(record, exporter);
-        records.push(record);
-    }
-    // console.log(records);
-    db.close((err)=>{console.log(err?.message)});
-});
+x.then((response)=>{
+    constructRecords(response).then((response)=>{
+        for(let record of response){
+            exporter.addCard(record);
+        }
+    })
+})

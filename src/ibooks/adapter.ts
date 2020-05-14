dfc79e2c807d5ddb49617bd94b26d26a405a062e
Krzysztof Czarnecki <kjczarne@Krzysztofs-MBP.home>
2020-05-15 00:25:31 +0200

    feat: using logger and the collection update function

--- src/ibooks/adapter.ts
+++ src/ibooks/adapter.ts
@@ -6,7 +6,9 @@ import * as yaml from 'yaml';
 import { AnkiEngine } from '../anki/ankiEngine';
 import { AnkiPackager } from '../anki/ankiPackager';
 import { models } from '../anki/ankiObjects';
-import { delay } from '../utils';
+import { IPDeck } from '../anki/interfaces';
+import { delay, logger } from '../utils';
+import { dedent } from 'ts-dedent';
 
 let envFile: string = readFileSync('./src/ibooks/env.yaml', 'utf8');
 let envObj: any = yaml.parse(envFile);
@@ -30,7 +32,23 @@ let x = sqlQueryRun(fullPath, sqlQuery);
 let db2Path: string = 'sranki.sqlite';
 let schema: string = readFileSync('./src/anki/ankiDbSchema.sql').toString()
 let now: number = Date.now().valueOf();
-let ankiEngine = new AnkiEngine('heck_deck', now, schema, db2Path, models);
+let deck: IPDeck = {
+    desc: "Insert Deck Description Here",
+    name: "New Deck",
+    extendRev: 10,
+    usn: -1,
+    collapsed: false,
+    newToday: [545, 0],  //TODO: should be handled by the engine (unchanged if found)
+    timeToday: [545, 0],  // TODO: should be handled by the engine (unchanged if found)
+    dyn: 0, // TODO: should be handled by the engine (unchanged if found)
+    extendNew: 10,
+    conf: 1,  // maps to DConf key
+    revToday: [545, 0], // TODO: should be handled by the engine (unchanged if found)
+    lrnToday: [545, 0], // TODO: should be handled by the engine (unchanged if found)
+    id: now,  // deck ID
+    mod: now
+}
+let ankiEngine = new AnkiEngine('heck_deck', deck, schema, db2Path, models);
 let ankiPackager = new AnkiPackager(db2Path, '');
 
 x.then((response)=>{
@@ -38,11 +56,23 @@ x.then((response)=>{
         for(let record of response){
             ankiEngine.addCard(record);
         }
+        ankiEngine.updateCollectionTable();
     })
 }).then(()=>{
     ankiPackager.pack().then((zipBuffer) => {
         writeFileSync(`${ankiEngine.deckName}.apkg`, zipBuffer, 'binary');
-        console.log(`Package has been generated: ${ankiEngine.deckName}.apkg`);
-      })
-      .catch((err) => {`Error: console.log(${err.message})`});
+        logger.log({
+            level: "silly",
+            message: dedent`
+            Package has been generated: ${ankiEngine.deckName}.apkg
+            `
+        });
+    }).catch((err) => {
+        logger.log({
+            level: "error",
+            message: dedent`
+            Error creating the .apkg file: ${err}
+            `
+        });
+    });
 })

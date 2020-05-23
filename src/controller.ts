bd6ce05508c861c2f79f5dc92e34657adba5e848
Krzysztof Czarnecki <kjczarne@Krzysztofs-MBP.home>
2020-05-23 15:57:08 +0200

    feat: added a controller module to run `refine` command
    This will be the main point of extension when writing new adapters. Adapter has to transform data from one source to an IRecord document and then enable transformation to a different format.

---
+++ src/controller.ts
@@ -0,0 +1,14 @@
+// use process.argv here, first two args are the node command elements (node, script path)
+import iBooks2Anki from './adapters/iBooks2Anki';
+
+switch (process.argv[2]){
+    case 'ibooks-anki':
+        iBooks2Anki(
+            process.argv[3],  // book name (input)
+            process.argv[4],  // deck name (output)
+            process.argv[5]   // optional: APKG path (output)
+        );
+        break;
+    default:
+        console.log("Called with wrong arguments!");
+}

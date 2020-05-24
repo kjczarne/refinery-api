12ec204d861fb73126384a17120270ae103bc3fc
Krzysztof Czarnecki <kjczarne@Krzysztofs-MBP.home>
2020-05-24 15:52:30 +0200

    feat: extended controller to handle iBooks2Refinery

--- src/controller.ts
+++ src/controller.ts
@@ -1,7 +1,13 @@
 // use process.argv here, first two args are the node command elements (node, script path)
+import iBooks2Refinery from './adapters/iBooks2Refinery';
 import iBooks2Anki from './adapters/iBooks2Anki';
 
 switch (process.argv[2]){
+    case 'ibooks':
+        iBooks2Refinery(
+            process.argv[3]
+        );
+        break;
     case 'ibooks-anki':
         iBooks2Anki(
             process.argv[3],  // book name (input)

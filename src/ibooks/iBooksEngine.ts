d3bb495bc6adb87efe96554001a1961357c74c54
Krzysztof Czarnecki <kjczarne@Krzysztofs-MBP.home>
2020-06-07 17:51:27 +0200

    fix: better RefineryDb handling (no implicit undefined)

--- src/ibooks/iBooksEngine.ts
+++ src/ibooks/iBooksEngine.ts
@@ -41,26 +41,51 @@ export class AppleiBooksEngine {
         this.recordsDb = new RefineryDatabaseWrapper(configPath);
     }
 
-    async load(bookName: string){
-        return sqlQueryRun(this.pathToLibraryDb, this._sqlQuery1).then((response1)=>{
-            sqlQueryRun(this.pathToAnnotationDb, this._sqlQuery2).then((response2)=>{
-                // filter out annotations that match the title in the function signature:
-                let filteredResponse1 = response1.filter((v)=>{v.bookTitle === bookName});
-                let filteredResponse2 = response2.filter((v)=>{filteredResponse1.includes(v.assetId)});
-                // add ebook type info to the response object:
-                filteredResponse2.forEach((v)=>{
-                    v['pageMap']={pagemapType: 'epubcfi', pagemapValue: v.pagemapValue};
-                    v['richContent']='';
-                    v['source']=bookName;
-                });
-                // construct records:
-                constructRecords(filteredResponse2).then((response)=>{
-                    response.forEach((v)=>{return JSON.stringify(v)});
-                    this.recordsDb.db?.bulk({docs: response});
+    async load(bookName: string): Promise<string> {
+        let pr: Promise<string> = new Promise<string>((resolve, reject)=>{
+            sqlQueryRun(this.pathToLibraryDb, this._sqlQuery1).then((response1)=>{
+                sqlQueryRun(this.pathToAnnotationDb, this._sqlQuery2).then((response2)=>{
+                    // filter out annotations that match the title in the function signature:
+                    var filteredResponse1 = response1.filter((v)=>{return v.bookTitle === bookName});
+                    let filteredResponse2 = response2.filter((v)=>{return filteredResponse1.filter((v2)=>{v2.assetId === v.assetId})});
+                    // add ebook type info to the response object:
+                    filteredResponse2.forEach((v)=>{
+                        v['pageMap']={pagemapType: 'epubcfi', pagemapValue: v.pagemapValue};
+                        v['richContent']='';
+                        v['source']=bookName;
+                    });
+                    // construct records:
+                    constructRecords(filteredResponse2).then((response)=>{
+                        response.forEach((v)=>{return JSON.stringify(v)});
+                        this.recordsDb.auth.then(()=>{
+                            this.recordsDb.db.bulk({docs: response}).then((res)=>{
+                                logger.log({
+                                    level: 'silly',
+                                    message:
+                                    `Added ${response} to RefineryDb.
+                                    Response: ${res}`
+                                })
+                            }).catch((err)=>{
+                                logger.log({
+                                    level: 'error',
+                                    message:
+                                    `Error PUTting docs into the RefineryDb: ${err}`
+                                });
+                            });
+                            resolve(response[0]._id);
+                        }).catch((err)=>{
+                            logger.log({
+                                level: 'error',
+                                message:
+                                `Problem accessing RefineryDb: ${err}`
+                            });
+                            reject(err);
+                        })
+                    });
                 });
             });
-        });
-        
+        })
+        return pr;
     }
 }
 

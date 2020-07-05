6a203cad78dc5062d85f947a9f5a9585da2d1573
Krzysztof Czarnecki <kjczarne@Krzysztofs-MBP.home>
2020-07-05 18:06:31 +0200

    chore: added index.ts for the package to be importable

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+export * from './interfaces';
+
+export { RefineryDatabaseWrapper } from './engine';
+
+export { 
+    constructRecord, 
+    constructRecords, 
+    convertToHtml,
+    convertToMarkdown, 
+    convert 
+} from './engine';
+
+export { Scheduler } from './scheduler';
+
+export { FlashcardRevisionController } from './controller';

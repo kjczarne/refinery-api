612fd59e6d5e0e8e62ce3bd4c5d06d87b58320c4
Krzysztof Czarnecki <kjczarne@Krzysztofs-MBP.home>
2020-09-07 00:30:54 +0200

    refactor: indentation

--- src/relays/ingressRelay.ts
+++ src/relays/ingressRelay.ts
@@ -2,22 +2,22 @@ import { logger } from '../utils';
 import BaseEngine from '../engines/baseEngine';
 
 let f = (
-    engine: BaseEngine,
-    resource: string,
-    batch?: string,
-    notebook?: string
-)=>{
-    engine.load(resource, batch, notebook).then(()=>{
-        logger.log({
-            level: 'info',
-            message: `${resource} loaded from ${BaseEngine.descriptor} to Refinery Database.`
-        });
-    }).catch((err)=>{
-        logger.log({
-            level: 'error',
-            message: `${resource} load failed from ${BaseEngine.descriptor}: ${err}`
-        })
+  engine: BaseEngine,
+  resource: string,
+  batch?: string,
+  notebook?: string
+) => {
+  engine.load(resource, batch, notebook).then(() => {
+    logger.log({
+      level: 'info',
+      message: `${resource} loaded from ${BaseEngine.descriptor} to Refinery Database.`
     });
+  }).catch((err) => {
+    logger.log({
+      level: 'error',
+      message: `${resource} load failed from ${BaseEngine.descriptor}: ${err}`
+    })
+  });
 }
 
 export default f;
